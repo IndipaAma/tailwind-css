4989331285b98403066a761ae0fb5c2314dcc038
IndipaAma <amanethindipa123@gmail.com>
2025-07-02 13:24:03 +0530

    Implemented the Hero Section without interactivity

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+import MainImage from "../../public/Illustration.svg";
+
+const HeroSection = () => {
+  return (
+    <div className="flex mt-10 py-20 items-center justify-center gap-50 ">
+      <div>
+        <h1 className="text-6xl font-semibold text-[#4D4D4D]">
+          Lessons and Insight
+        </h1>
+        <h2 className="text-6xl font-semibold text-[#43A046]">From 8 Years</h2>
+        <h3 className="text-[#717171]">
+          Where to grow your business as a photographer: site or social media?
+        </h3>
+        <button className="mt-10 bg-[#43A046] text-amber-50 px-10 py-3.5 rounded-md">
+          Register
+        </button>
+      </div>
+      <div>
+        <Image src={MainImage} alt="Main-Image"></Image>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
